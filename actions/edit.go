a5fcaa84895658e783b3b1a7736fb8247748ab87
Daniel Lazar <lazardani0112@gmail.com>
2024-03-01 22:08:09 +0100

    actions/edit: complete edit funtionality

--- actions/edit.go
+++ actions/edit.go
@@ -1,13 +1,25 @@
 package actions
 
 import (
-	"fmt"
+	"log"
+	"time"
 
 	"github.com/ducktordanny/td/config"
 	"github.com/ducktordanny/td/flags"
 )
 
 func Edit(scope flags.Scope, value *string) {
-	result := config.GetContentByEditor("Stuff to edit for now.")
-	fmt.Println(result)
+	if len(*value) != 7 {
+		log.Fatalln("Invalid Sha ID.")
+	}
+	conf := config.ReadConfig()
+	list := config.GetListBasedOnScope(scope, &conf)
+	indexToEdit := config.GetIndexOfListBySha(*list, *value)
+	if indexToEdit == -1 {
+		log.Fatalln("Invalid Sha ID.")
+	}
+	result := config.GetContentByEditor((*list)[indexToEdit].Content)
+	(*list)[indexToEdit].Content = result
+	(*list)[indexToEdit].CreatedAt = time.Now()
+	config.WriteConfig(conf)
 }
